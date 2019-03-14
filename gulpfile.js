989eaf7e90c7c8d67d232118fbe911b6ead9ad9a
Aileen Nowak <AileenCGN@gmail.com>
2019-03-14 14:45:22 +0800

    ⬆️  Update Gulp to v4 and make livereload work
    no issue
    
    - update Gulp and related dependencies to v4
    - Refactor gulpfile for Gulp 4.0
    - Make livereload work

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,57 @@
-var gulp = require('gulp');
+const { series, watch, src, dest } = require('gulp');
+const pump = require('pump');
 
 // gulp plugins and utils
-var gutil = require('gulp-util');
 var livereload = require('gulp-livereload');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
+var beeper = require('beeper');
 
-var swallowError = function swallowError(error) {
-    gutil.log(error.toString());
-    gutil.beep();
-    this.emit('end');
-};
+function serve(done) {
+    livereload.listen();
+    done();
+}
 
-var nodemonServerInit = function () {
-    livereload.listen(1234);
+const handleError = (done) => {
+    return function (err) {
+        if (err) {
+            beeper();
+        }
+        return done(err);
+    };
 };
 
 sass.compiler = require('node-sass');
 
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
-});
+function css(done) {
+    pump([
+        src('./assets/main/sass/*.scss', { sourcemaps: true }),
+        sass({ outputStyle: 'compressed' }).on('error', sass.logError),
+        dest('assets/main/css', { sourcemaps: './' }),
+        livereload()
+    ], handleError(done));
+}
 
-gulp.task('generate', ['css']);
-
-gulp.task('css', function () {
-    return gulp.src('./assets/main/sass/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('assets/main/css/'))
-        .pipe(livereload());
-});
-
-gulp.task('watch', function () {
-    gulp.watch('./assets/main/sass/**/**', ['css']);
-});
-
-gulp.task('zip', ['css'], function () {
+function zipper(done) {
     var targetDir = 'dist/';
     var themeName = require('./package.json').name;
     var filename = themeName + '.zip';
 
-    return gulp.src([
-        '**',
-        '!node_modules', '!node_modules/**',
-        '!dist', '!dist/**'
-    ])
-        .pipe(zip(filename))
-        .pipe(gulp.dest(targetDir));
-});
+    pump([
+        src([
+            '**',
+            '!node_modules', '!node_modules/**',
+            '!dist', '!dist/**'
+        ]),
+        zip(filename),
+        dest(targetDir)
+    ], handleError(done));
+}
 
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
-});
+const watcher = () => watch('./assets/main/sass/**/**', css);
+const build = series(css);
+const dev = series(build, serve, watcher);
+
+exports.build = build;
+exports.zip = series(build, zipper);
+exports.default = dev;
